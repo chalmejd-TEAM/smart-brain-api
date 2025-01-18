641b0e64838d59bc5ef39762e514dcfb4284d170
chamejd <chalmejd@protonmail.com>
2025-01-18 10:25:24 -0600

    Added db access to /profile/:id route

--- server.js
+++ server.js
@@ -74,16 +74,17 @@ app.post('/register', (req, res)=> {
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        } 
+    db.select('*').from('users').where({
+        id: id
     })
-    if (!found) {
-        res.status(400).json('No such user');
-    }
+    .then(user => {
+        if (user.length) {
+            res.json(user[0])
+        } else {
+            res.status(400).json('User not found')
+        }
+    })
+    .catch(err => res.status(400).json('Error getting user'))
 })
 
 app.put('/image', (req, res)=> {
